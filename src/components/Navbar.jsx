ba102fb06d2982a3f73e83b236cf88929b78953d
anvar-muzaffarli <prof.anvar2013@gmail.com>
2023-02-05 11:38:02 +0400

    Navbar tamamlandi ve useRef oyredildi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import brendinLogosu from '../assets/images/logo.svg'
 import './Navbar.css'
 // iconlarin daxil edilmesi
@@ -9,10 +9,29 @@ import Button from './Button'
 
 const Navbar = () => {
     // JSX DE DEYISHENLER NEYIN ICERISINDE QEYD OLUNUR? {}
+  const overlayDivininUnvani = useRef()
+  // console.log(overlayDivininUnvani.current)
+    const openOverlayMenu = (e) => {
+      const kliklediyimYer = e.target
+
+      if(kliklediyimYer.classList.contains('bars-icon')) {
+        overlayDivininUnvani.current.classList.add('aktiv')
+      }
+    }
+
+    const closeOverlayMenu = (e) =>  {
+      const kliklediyimYer = e.target
+
+      if(kliklediyimYer.classList.contains('cross-icon')) {
+        overlayDivininUnvani.current.classList.remove('aktiv')
+
+      }
+    }
+    
   return (
     <>
-    <div className="overlay">
-        <GiCrossedSwords className='cross-icon' />
+    <div className="overlay" ref={overlayDivininUnvani}>
+        <GiCrossedSwords className='cross-icon' onClick={closeOverlayMenu}/>
 
         <div className="nav-links">
             <a href="">Home</a>
@@ -26,7 +45,7 @@ const Navbar = () => {
   <div className="container-fluid">
     <a className="navbar-brand" href="#"><img src={brendinLogosu} alt="Berber " /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <AiOutlineBars className='bars-icon' />
+    <AiOutlineBars className='bars-icon' onClick={openOverlayMenu} />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
---
+++ src/demo.js
@@ -0,0 +1,12 @@
+function sayHello (name) {
+return "Salam" + name
+}
+
+const sayHello2 = name =>  "Salam" + name
+
+telebe1 ={
+    ad: "Aygun",
+    soyad: "Xuduyeva"
+}
+
+telebe1.ad + telebe1.soyad
